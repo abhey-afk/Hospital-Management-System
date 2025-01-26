10b29937dba853a1e151b653f1190f701f2c621c
Abhey Mishra <97222685+abhey-afk@users.noreply.github.com>
2025-01-26 19:46:14 +0530

    Add files via upload

---
+++ Backend/app.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+
+const app = express();
+
+// Load environment variables
+config({ path: "./config/.env" });
+
+// Middleware configuration
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+// Routers
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+// Connect to the database
+dbConnection();
+
+// Global error middleware
+app.use(errorMiddleware);
+
+// Export the app
+export default app;
+
+
